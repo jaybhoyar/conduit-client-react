714fa2021958b6829c49719fb71a42ee88ce0436
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-14 18:16:18 +0530

    header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-
+// import { Link } from "react-router-dom";
+import "../../styles/common.scss";
 function Header() {
-	return <div>
-    
-  </div>;
+	return (
+		<div className="header_container">
+			<header className="header">
+				<div className="logo">
+					<p>Conduit</p>
+				</div>
+				<div className="search_box">
+					<input type="text" name="" id="" />
+				</div>
+				<div className="auth_button">
+					<a href="#">Home</a>
+					<a href="#">Sign in</a>
+					<a href="#">Signup</a>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default Header;
